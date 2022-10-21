4b6492ee67e37d14ff7bcf1ea5a0cd93525a4660
kapadokia <ktnyandoch@gmail.com>
2022-10-21 08:09:50 +0300

    solved first ruby program

---
+++ app.rb
@@ -0,0 +1,13 @@
+# puts "Hello World!"
+# print "Pass this test, please."
+# p [1,2,3]
+
+$firstName ="Hello World!"
+
+def printName
+    puts "Hello #{$firstName}"
+    print "Pass this test, please."
+    p [1,2,3]
+end
+
+printName
---
+++ lessons.rb
@@ -0,0 +1,11 @@
+# ruby will run weather the params has curly brackets or don't
+# def add num1,num2
+#    puts num1
+#    puts num2
+#    num1+num2
+# end
+
+# # Methods printing
+# puts add(2,3)+add(4,5)
+
+# METHODS
